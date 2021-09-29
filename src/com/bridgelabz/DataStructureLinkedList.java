fd7238d8ea3ae13e2669f714eeb062aebd0654b4
yogkri <dharmayog91@gmail.com>
2021-09-29 18:46:56 +0530

    Adding Pop Method To Head Node

--- src/com/bridgelabz/DataStructureLinkedList.java
+++ src/com/bridgelabz/DataStructureLinkedList.java
@@ -4,6 +4,7 @@ import javax.xml.crypto.Data;
 
 /* linked list learing problem
  * create a node class and perform compute on the linked list
+ * create a linked list and pop the head node
  */
 public class DataStructureLinkedList {
     static Node head;
@@ -31,40 +32,14 @@ public class DataStructureLinkedList {
             last.next = newNode;
         }
     }
-    static void insertAtMid(int x)
-    {
-        // if list is empty
-        if (head == null)
-            head = new Node(x);
+    //pop method to set head to next node
+    public static void pop(){
+        Node temp =head;
+        if(head == null) {
+            System.out.println("linked list does not have data");
+        }
         else {
-            // get a new node
-            Node newNode = new Node(x);
-
-            Node ptr = head;
-            int len = 0;
-
-            // calculate length of the linked list
-            //, i.e, the number of nodes
-            while (ptr != null) {
-                len++;
-                ptr = ptr.next;
-            }
-
-            // 'count' the number of nodes after which
-            // the new node is to be inserted
-            int count = ((len % 2) == 0) ? (len / 2) :
-                    (len + 1) / 2;
-            ptr = head;
-
-            // 'ptr' points to the node after which
-            // the new node is to be inserted
-            while (count-- > 1)
-                ptr = ptr.next;
-
-            // insert the 'newNode' and adjust
-            // the required links
-            newNode.next = ptr.next;
-            ptr.next = newNode;
+            head = head.next;
         }
     }
 
@@ -85,14 +60,14 @@ public class DataStructureLinkedList {
     public static void main(String args[]) {
         System.out.println("welcome to linked list");
         //create obejct and give to the method
-        head = null;
-        head = new Node(56);
-        head.next = new Node(70);
-        /*DataStructureLinkedList linkedList = new DataStructureLinkedList();
+        DataStructureLinkedList linkedList = new DataStructureLinkedList();
         linkedList.addNode(56);
+        linkedList.addNode(30);
         linkedList.addNode(70);
-        System.out.println("Enter the node miidle of the linked list"); */
-        insertAtMid(30);
+        display();
+        System.out.println("linked list data");
+        //pop method point the next node
+        linkedList.pop();
         display();
 
     }
