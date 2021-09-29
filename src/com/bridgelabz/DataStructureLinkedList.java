e6a3e9c2b0258dc369b8692b3582ce8893c8dce0
yogkri <dharmayog91@gmail.com>
2021-09-29 18:38:22 +0530

    Adding The Node In Miiddle Of Linked List

--- src/com/bridgelabz/DataStructureLinkedList.java
+++ src/com/bridgelabz/DataStructureLinkedList.java
@@ -1,33 +1,75 @@
 package com.bridgelabz;
 
+import javax.xml.crypto.Data;
+
+/* linked list learing problem
+ * create a node class and perform compute on the linked list
+ */
 public class DataStructureLinkedList {
-        Node head;
-        //create a node for the linked list
-        static class Node{
-            int data;
-            Node next;
-            Node(int d){
-                data = d;
-                next = null;
-            }
+    static Node head;
+    //create a node for the linked list
+    static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data = d;
+            next = null;
         }
+    }
     // create the add method the give the value linked list
-    public void addNode(int data){
+    public void addNode(int data) {
         Node newNode = new Node(data);
         newNode.next = null;
-        if(this.head == null){
+        if (this.head == null) {
             head = newNode;
         }
-        else{
+        else {
             Node last = this.head;
-            while(last.next != null) {
+            while (last.next != null) {
                 last = last.next;
             }
             last.next = newNode;
         }
     }
+    static void insertAtMid(int x)
+    {
+        // if list is empty
+        if (head == null)
+            head = new Node(x);
+        else {
+            // get a new node
+            Node newNode = new Node(x);
+
+            Node ptr = head;
+            int len = 0;
+
+            // calculate length of the linked list
+            //, i.e, the number of nodes
+            while (ptr != null) {
+                len++;
+                ptr = ptr.next;
+            }
+
+            // 'count' the number of nodes after which
+            // the new node is to be inserted
+            int count = ((len % 2) == 0) ? (len / 2) :
+                    (len + 1) / 2;
+            ptr = head;
+
+            // 'ptr' points to the node after which
+            // the new node is to be inserted
+            while (count-- > 1)
+                ptr = ptr.next;
+
+            // insert the 'newNode' and adjust
+            // the required links
+            newNode.next = ptr.next;
+            ptr.next = newNode;
+        }
+    }
+
     //display the data to linked list
-    public void display(){
+    public static void display(){
         if(head == null){
             System.out.println("linked list has no value");
         }
@@ -43,11 +85,15 @@ public class DataStructureLinkedList {
     public static void main(String args[]) {
         System.out.println("welcome to linked list");
         //create obejct and give to the method
-        DataStructureLinkedList linkedList = new DataStructureLinkedList();
-        linkedList.addNode(70);
-        linkedList.addNode(30);
+        head = null;
+        head = new Node(56);
+        head.next = new Node(70);
+        /*DataStructureLinkedList linkedList = new DataStructureLinkedList();
         linkedList.addNode(56);
-        linkedList.display();
+        linkedList.addNode(70);
+        System.out.println("Enter the node miidle of the linked list"); */
+        insertAtMid(30);
+        display();
 
     }
 }
